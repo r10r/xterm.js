af0b3fcd3a6320d7bfd8478e858453e0de34dd45
Ruben Jenster <r.jenster@drachenfels.de>
2022-05-09 14:09:30 +0000

    demo server: fix printing listening host

--- demo/server.js
+++ demo/server.js
@@ -136,7 +136,7 @@ function startServer() {
   var port = process.env.PORT || 3000,
       host = os.platform() === 'win32' ? '127.0.0.1' : '0.0.0.0';
 
-  console.log('App listening to http://127.0.0.1:' + port);
+  console.log('App listening to http://' + host + ":" + port);
   app.listen(port, host);
 }
 
